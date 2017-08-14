6a3ddd06ddd126ea49e0595c96eee753330b2164
Amir Abdullaev <acrofus@gmail.com>
2017-08-14 23:54:45 +0300

    Solve 1.8

---
+++ 1_8.scm
@@ -0,0 +1,19 @@
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess (improve guess x))
+      guess
+      (cbrt-iter (improve guess x)
+                 x)))
+
+
+(define (improve guess x)
+  (/ (+ (/ x (* guess guess)) 
+        (* 2 guess)) 
+     3))
+
+
+(define (good-enough? guess next-guess)
+  (< (abs (- next-guess guess)) 0.001))
